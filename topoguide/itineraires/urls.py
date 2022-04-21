05dec81e33a291cf860485e804013f2c4b6f2b5a
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-21 04:36:40 +0200

    add dummy views and route them
    Add dummy views for list of itineraires, list of sorties, editing a sortie, creating a sortie, viewing a sortie
    
    Routes are defined in  files

---
+++ topoguide/itineraires/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.itineraire, name="itineraire_list"),
+    path('sorties/<int:itineraire_id>/', views.sorties, name = "sorties_list"),
+    
+    path('sortie/<int:sortie_id>', views.sortie, name="sortie_details"),
+    path("modif_sortie/<int:sortie_id>/", views.modif_sortie, name="modif_sortie"),
+    path("nouvelle_sortie/", views.nouvelle_sortie, name="create_sortie"),
+]
--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def itineraire(request):
+    return HttpResponse("Liste des itinéraires")
+
+def sorties(request, itineraire_id):
+    return HttpResponse("Liste des sorties de l'itinéraire %s" % itineraire_id)
+
+def sortie(request, sortie_id):
+    return HttpResponse("Détail de la sortie %s" % sortie_id)
+
+def nouvelle_sortie(request):
+    return HttpResponse("Page d'entrée de sortie")
+
+def modif_sortie(request, sortie_id):
+    return HttpResponse("Page de modification de la sortie %s" % sortie_id)
