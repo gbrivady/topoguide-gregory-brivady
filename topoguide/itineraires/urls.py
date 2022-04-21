20f3af4848a285abd7ad46bf8504319a0a235066
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-21 08:40:24 +0200

    add app namespace
    Add app namespace to allow reverse-search

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "itineraires"
 urlpatterns = [
     path('', views.IndexView.as_view(), name="itineraire_list"),
     path('sorties/<int:itineraire_id>/', views.sorties, name = "sorties_list"),
