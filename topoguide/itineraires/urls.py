7c4a7c796e4c5a0119a0548060a694bdb14b2ea3
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-22 02:26:00 +0200

    Route to sortie edit page

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     path('sortie/<int:pk>', views.SortieView.as_view(), name="sortie"),
     
-    path("modif_sortie/<int:sortie_id>/", views.modif_sortie, name="modif_sortie"),
     path("nouvelle_sortie/", views.NewSortie.as_view(), name="nouvelle_sortie"),
+    path("modif_sortie/<int:pk>/", views.EditSortie.as_view(), name="modif_sortie"),
+
 ]
